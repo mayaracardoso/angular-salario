7ef4ce1d5fb266a3fad273fd578802132e81c3a5
Mayara Cardoso <mayarakardoso@gmail.com>
2019-06-04 21:52:26 -0300

    angular-salario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Salario } from 'src/salario';
+import { interval } from 'rxjs';
+import { takeUntil, map, filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-salario';
+  salarioInformado: number;
+  salarioLiquidoDesejado: number;
+
+  salario = new Salario(0);
+
+ 
+  public updateSalario(event): void {
+    this._internalUpdateSalario(+event.target.value);
+  }
+
+  public _internalUpdateSalario(newSalario): void {
+    this.salario = new Salario(+newSalario);
+  }
+
+
+  updateSalarioLiquidoDesejado(event): void {
+    this.salarioLiquidoDesejado = event.target.value;
+  }
+
+  public converteSalarioBrutoParaLiquido(): void {
+    this.salarioInformado = this.salarioLiquidoDesejado;    
+    const obs$ = interval(1).pipe(
+      map(() => {
+        const currentValue = +this.salario.salarioLiquido;
+        const difference = Math.abs(currentValue - +this.salarioLiquidoDesejado);
+        const increment = difference >= 5 ? 1 : 0.01;
+        this.salarioInformado = +((this.salarioInformado + increment).toFixed(2));
+
+        this._internalUpdateSalario(this.salarioInformado);
+        return +this.salario.salarioLiquido;
+      }),
+    );
+
+    const match$ = obs$.pipe(
+      filter(currentValue => +currentValue >= +this.salarioLiquidoDesejado),
+    );
+    obs$.pipe(takeUntil(match$)).subscribe();
+  }
 }
+
